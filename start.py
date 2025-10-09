1c14c8713c37ee3b0860b0121d1dfa02864068d8
Ludwig Lehnert <58045323+lehlud@users.noreply.github.com>
2025-10-09 13:22:45 +0200

    initial commit

---
+++ start.py
@@ -0,0 +1,139 @@
+import art
+import time
+import curses
+import random
+import subprocess as sp
+
+FONTS = ['poison', 'banner3', 'pebbles', 'colossal', 'lildevil', 'ghost', 'swampland', 'larry3d', 'nancyj-fancy', 'slide', 'tanja', 'jazmine']
+FONT = random.choice(FONTS)
+# FONT = random.choice(art.ASCII_FONTS)
+
+def write_centered(scr, texts: list[str]):
+    height, width = scr.getmaxyx()
+
+    total_lines = sum(len(text.split('\n')) - 1 for text in texts)
+    start_y = 0 # (height - total_lines) // 2
+
+    for i, text in enumerate(texts):
+        lines = text.split('\n')
+
+        max_line_width = max(len(line) for line in lines)
+        start_x = (width - max_line_width) // 2
+
+        for j, line in enumerate(lines):
+            try: scr.addstr(start_y + j, start_x, line, curses.A_BOLD)
+            except: pass
+
+        start_y += len(lines)
+        start_y += 1
+
+def write_status(scr, heading: str, duration_s: int, remaining_s: int, paused: bool):
+    art_heading = art.text2art(heading, font='small')
+
+    is_break = duration_s != 25 * 60
+
+    remaining_m = str(remaining_s // 60)
+    remaining_s = str(remaining_s % 60)
+    art_remaining = art.text2art(f'{remaining_m.rjust(2, "0")}:{remaining_s.rjust(2, "0")}', font=FONT)
+
+    write_centered(scr, ['', art_heading, art_remaining, 'BREAK' if is_break else 'WORKY WORK', '(PAUSED)' if paused else ''])
+    
+
+def main(scr):
+    try:
+        curses.curs_set(0)
+        scr.nodelay(True)
+        scr.timeout(200)
+
+        started = time.time()
+        duration_s = 25 * 60
+        remaining_duration_s = duration_s
+
+        notification_id = None
+        notifiers = [(600, '10 Minutes'), (120, '2 Minutes'), (60, '1 Minutes'), (30, '30 Seconds'), (10, '10 Seconds'), (5, '5 Seconds'), (3, '3 Seconds'), (2, '2 Seconds'), (1, '1 Seconds')]
+
+        paused = False
+        triggered_end = False
+        sent_notifiers = []
+
+        def reset(new_duration: int):
+            nonlocal duration_s, remaining_duration_s, paused, triggered_end, sent_notifiers, started
+            duration_s = new_duration
+            remaining_duration_s = duration_s
+
+            paused = False
+            triggered_end = False
+            sent_notifiers = []
+
+            started = time.time()
+
+        def pause():
+            nonlocal paused, remaining_duration_s, duration_s
+            if paused: return
+            paused = True
+            remaining_duration_s = duration_s
+            
+
+        def resume():
+            nonlocal paused, started
+            if not paused: return
+            paused = False
+            started = time.time()
+
+        while True:
+            is_break = duration_s != 25 * 60
+            
+            remaining_s = int((remaining_duration_s - time.time() + started) // 1) if not paused else remaining_duration_s
+            if remaining_s < 0: remaining_s = 0
+
+            if not triggered_end and remaining_s == 0:
+                triggered_end = True
+            
+                if is_break:
+                    sp.Popen(['ffplay', 'bells.mp3', '-v', '0', '-nodisp', '-autoexit'])
+                    sp.Popen(['xdg-screensaver', 'reset'])
+                else:
+                    sp.Popen(['xdg-screensaver', 'activate'])
+                    reset(5 * 60)
+                    continue
+
+            for notifier_s, notifier_msg in notifiers:
+                if notifier_s != remaining_s: continue
+                if notifier_s in sent_notifiers: break
+
+                args = ['notify-send', 'Pomodoro!', f'{notifier_msg} remaining', '-p', '-t', '5000']
+                if notification_id: args += ['-r', notification_id]
+                stdout, _ = sp.Popen(args, stdout=sp.PIPE).communicate()
+                notification_id = stdout.strip()
+
+                sent_notifiers.append(notifier_s)
+
+                break
+
+            scr.clear()
+            write_status(scr, 'Pomodoro!', duration_s, remaining_s, paused)
+            scr.refresh()
+
+            key = scr.getch()
+            if key == -1: continue
+
+            if key == ord('q'):
+                exit(0)
+                continue
+            
+            if key == ord(' '):
+                if paused and not is_break:
+                    resume()
+                    continue
+                
+                reset(25 * 60)
+                if not is_break: pause()
+                continue
+    
+    except KeyboardInterrupt:
+        exit(0)
+            
+ 
+
+if __name__ == '__main__':
+    curses.wrapper(main)
